8b26caded67ceb6b246df2bdaebe28045d4ef07c
varunkumarmedam <varunkumarmedam@gmail.com>
2023-03-29 01:32:04 +0530

    added table_calendar

--- lib/main.dart
+++ lib/main.dart
@@ -2,23 +2,21 @@ import 'dart:async';
 import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:myapp/MainCalendar.dart';
 
 void main() {
-  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-      statusBarColor: Colors.transparent
-  ));
+  SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(statusBarColor: Colors.transparent));
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-
         primarySwatch: Colors.orange,
       ),
       home: SplashScreen(),
@@ -26,9 +24,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
-class SplashScreen extends StatefulWidget{
-
+class SplashScreen extends StatefulWidget {
   @override
   _SplashScreenState createState() => _SplashScreenState();
 }
@@ -36,17 +32,13 @@ class SplashScreen extends StatefulWidget{
 class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
-
     super.initState();
-    Timer(Duration(seconds: 5),
-            ()=>Navigator.pushReplacement(context,
-            MaterialPageRoute(builder:
-                (context) =>
-                HomeScreen()
-            )
-        )
-    );
+    Timer(
+        Duration(seconds: 5),
+        () => Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => HomeScreen())));
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -57,9 +49,10 @@ class _SplashScreenState extends State<SplashScreen> {
           gradient: LinearGradient(
               begin: Alignment.topRight,
               end: Alignment.bottomLeft,
-
-              colors: [Color(0xFFFF800B),Color(0xFFCE1010),]
-          ),
+              colors: [
+                Color(0xFFFF800B),
+                Color(0xFFCE1010),
+              ]),
         ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
@@ -72,7 +65,9 @@ class _SplashScreenState extends State<SplashScreen> {
                   height: 300.0,
                   width: 300.0,
                 ),
-                Text("Life Lately",textAlign:TextAlign.center,
+                Text(
+                  "Life Lately",
+                  textAlign: TextAlign.center,
                   style: TextStyle(
                     color: Colors.white,
                     fontWeight: FontWeight.bold,
@@ -81,9 +76,8 @@ class _SplashScreenState extends State<SplashScreen> {
                 ),
               ],
             ),
-
             CircularProgressIndicator(
-              valueColor:  AlwaysStoppedAnimation<Color>(Colors.orange),
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.orange),
             ),
           ],
         ),
@@ -91,14 +85,34 @@ class _SplashScreenState extends State<SplashScreen> {
     );
   }
 }
+
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title:Text("Hello...")),
+      appBar: AppBar(title: Text("Hello...")),
+      floatingActionButton: IconButton(
+          onPressed: () => {
+                Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => MainCalendar()))
+              },
+          icon: Icon(Icons.calendar_month_outlined)),
       body: Center(
-          child:Text("Home page",textScaleFactor: 2,)
-      ),
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "Home page",
+            textScaleFactor: 2,
+          ),
+          TextButton(
+              onPressed: () => {
+                    Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) => MainCalendar()))
+                  },
+              child: Text("Open My Calendart"))
+        ],
+      )),
     );
   }
 }
